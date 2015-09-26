06a84653928cda12fadc81d1e581aa4828a79d48
ubuntu-isucon <kuogsi@gmail.com>
2015-09-26 13:45:35 +0900

    Add conf files

--- symbolic.sh
+++ symbolic.sh
@@ -4,3 +4,4 @@ ln -s ./supervisord.conf /etc/supervisord.conf
 ln -s ./limits.conf /etc/security/limits.conf
 ln -s ./sysctl.conf /etc/sysctl.conf
 ln -s ./env.sh /home/isucon/env.sh
+ln -s ./webapp/go/app /home/isucon/isucon5-qualifier/webapp/go/app
