182ec85579ea55b0110679935d1058676ffd0792
Kosugiyama <kuogsi@gmail.com>
2015-09-26 09:54:16 +0900

    Added symbolic script

---
+++ symbolic.sh
@@ -0,0 +1,5 @@
+# config file
+ln -s ./nginx.conf /etc/nginx/nginx.conf
+ln -s ./supervisord.conf /etc/supervisord.conf
+ln -s ./limits.conf /etc/security/limits.conf
+ln -s ./sysctl.conf /etc/sysctl.conf
