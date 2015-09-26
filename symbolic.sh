ad87cf6d3a0c4adeecdb8e8225b45dc676f5cd0e
ubuntu-isucon <kuogsi@gmail.com>
2015-09-26 11:17:20 +0900

    Added webapp and env.sh

--- symbolic.sh
+++ symbolic.sh
@@ -3,3 +3,4 @@ ln -s ./nginx.conf /etc/nginx/nginx.conf
 ln -s ./supervisord.conf /etc/supervisord.conf
 ln -s ./limits.conf /etc/security/limits.conf
 ln -s ./sysctl.conf /etc/sysctl.conf
+ln -s ./env.sh /home/isucon/env.sh
